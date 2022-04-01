44f5dccc5a835d8e56cb2348afdd4f852c7639b7
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-04-01 19:22:25 +0900

    delete: 不要なファイルを削除

--- src/components/template/Home/hooks/useChartData/modules/generateRandomColorCode/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * ランダムなカラーコードを返す関数
- */
-export const generateRandomColorCode = () => {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16)
-}
--- src/components/template/Home/hooks/useChartData/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type PopulationDataCache = {
-  [key: string]: { [key: string]: number }
-}
-
-export type CheckedDataType = { prefCode: string; prefName: string; colorCode: string }
